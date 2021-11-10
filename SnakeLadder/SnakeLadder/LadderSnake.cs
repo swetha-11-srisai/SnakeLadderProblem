44518af8e471bc7e9e21e18229d7472cf058c96b
swetha-11-srisai <swethachowdary857@gmail.com>
2021-11-10 13:40:07 -0800

    [Swetha] add : Player Starts at Position Zero

---
+++ SnakeLadder/SnakeLadder/LadderSnake.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLadder
+{
+    class LadderSnake
+    {
+        public const int PLAYER_POSITION = 0;
+        public void Position()
+        {
+            Random random = new Random();
+            if (random.Next(0, 1) == PLAYER_POSITION)
+            {
+                Console.WriteLine("the player is at starting Position");
+            }
+        }
+    }
+}
