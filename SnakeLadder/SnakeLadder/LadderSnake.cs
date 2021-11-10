fc88a13e3c005a1c2113c0875e56d19927d86bdd
swetha-11-srisai <swethachowdary857@gmail.com>
2021-11-10 14:16:29 -0800

    [Swetha] add : Player winning position

--- SnakeLadder/SnakeLadder/LadderSnake.cs
+++ SnakeLadder/SnakeLadder/LadderSnake.cs
@@ -6,34 +6,37 @@ namespace SnakeLadder
 {
     class LadderSnake
     {
-        public const int NO_PlAY = 0, LADDER = 1, SNAKE = 2;
+        public const int NO_PlAY = 0, LADDER = 1, SNAKE = 2, WINNING = 100;
         int PlayerPosition;
         public int DiceRoll()
         {
             Random random = new Random();
             return random.Next(1, 7);
         }
-        public void PlayerCheck()
+        public void Check()
         {
-            Random random = new Random();
-            int result = random.Next(0, 3);
-            switch (result)
+            while (PlayerPosition < 100)
             {
-                case NO_PlAY:
-                    break;
-                case LADDER:
-                    PlayerPosition += DiceRoll();
-                    break;
-                case SNAKE:
-                    PlayerPosition -= DiceRoll();
-                    if (PlayerPosition < 0)
-                    {
-                        PlayerPosition = 0;
-                    }
-                    break;
+                Random random = new Random();
+                int result = random.Next(0, 3);
+                switch (result)
+                {
+                    case NO_PlAY:
+                        break;
+                    case LADDER:
+                        PlayerPosition += DiceRoll();
+                        break;
+                    case SNAKE:
+                        PlayerPosition -= DiceRoll();
+                        if (PlayerPosition < 0)
+                        {
+                            PlayerPosition = 0;
+                        }
+                        break;
+                }
+
+
             }
-
-
         }
     }
 }
