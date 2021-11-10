4b8a5d253ed95aa684669519498ff7d4c046edcf
swetha-11-srisai <swethachowdary857@gmail.com>
2021-11-10 14:23:39 -0800

    [Swetha] add : Player exact winning position

--- SnakeLadder/SnakeLadder/LadderSnake.cs
+++ SnakeLadder/SnakeLadder/LadderSnake.cs
@@ -6,14 +6,14 @@ namespace SnakeLadder
 {
     class LadderSnake
     {
-        public const int NO_PlAY = 0, LADDER = 1, SNAKE = 2, WINNING = 100;
+        public const int NO_PlAY = 0, LADDER = 1, SNAKE = 2, WINNING = 100,START=0;
         int PlayerPosition;
         public int DiceRoll()
         {
             Random random = new Random();
             return random.Next(1, 7);
         }
-        public void Check()
+        public void CheckPosition()
         {
             while (PlayerPosition < 100)
             {
@@ -25,10 +25,14 @@ namespace SnakeLadder
                         break;
                     case LADDER:
                         PlayerPosition += DiceRoll();
+                        if (PlayerPosition > WINNING)
+                        {
+                            PlayerPosition -= DiceRoll();
+                        }
                         break;
                     case SNAKE:
                         PlayerPosition -= DiceRoll();
-                        if (PlayerPosition < 0)
+                        if (PlayerPosition < START)
                         {
                             PlayerPosition = 0;
                         }
